f0211f7945d2dab8e701ad94b1ab54d58ba5b4eb
Riley <107502823+theKrillionaire@users.noreply.github.com>
2026-02-24 16:41:53 -0600

    better collision fuuuck yeaaahh
    seperated collision into X and Y because it pmo

--- player.cpp
+++ player.cpp
@@ -6,7 +6,8 @@ void player::update(Rectangle* walls, int wallcount) {
 		if(walkFrame < 21) walkFrame++;
 		else walkFrame = 0;
 	} else walkFrame = 0;
-	touchingWall = 0;
+	touchingWallX = 0;
+	touchingWallY = 0;
 	if(IsKeyDown(KEY_LEFT)) { dir.x = -1; } 
 	else if(IsKeyDown(KEY_RIGHT)) { dir.x = 1; }
 	else { dir.x = 0; }
@@ -16,13 +17,17 @@ void player::update(Rectangle* walls, int wallcount) {
 	if(IsKeyDown(KEY_Z)) speed = 5;
 	else speed = 2.5;
 	collider = { pos.x, pos.y, collider.width,collider.height };
-	checkCollider = { pos.x + dir.x * speed, pos.y + dir.y * speed, collider.width,collider.height };
+	checkColliderX = { pos.x + dir.x * speed, pos.y, collider.width,collider.height };
+	checkColliderY = { pos.x, pos.y + dir.y * speed, collider.width,collider.height };
 	for(int i = 0; i < wallcount; i++) {
-		if(CheckCollisionRecs(checkCollider,walls[i])) touchingWall = 1;
+		if(CheckCollisionRecs(checkColliderX,walls[i])) touchingWallX = 1;
+		if(CheckCollisionRecs(checkColliderY,walls[i])) touchingWallY = 1;
 	}
-	if(!touchingWall) {
-		pos.y += dir.y * speed;
+	if(!touchingWallX) {
 		pos.x += dir.x * speed;
+	}
+	if (!touchingWallY) {
+		pos.y += dir.y * speed;
 	}
 }
 Vector2 player::getPos() { return pos; }
--- player.h
+++ player.h
@@ -8,9 +8,11 @@ class player {
 		Vector2 pos = {170,120};
 		Vector2 dir = {0, 0};
 		Rectangle collider = { 170,120,21,29 };
-		Rectangle checkCollider = { 170,120,20,24 };
+		Rectangle checkColliderX = { 170,120,20,24 };
+		Rectangle checkColliderY = { 170,120,20,24 };
 		float speed = 2.5;
-		int touchingWall = 0;
+		int touchingWallX = 0;
+		int touchingWallY = 0;
 		int walkFrame = 0;
 	public: 
 		void update(Rectangle* walls, int wallcount);
